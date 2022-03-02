8ae27fef5a5bab0a64dcd3b7c6caaa9654c21487
manirul islam <manirul26@gmail.com>
2022-03-02 08:18:12 +0600

    March 02 2022

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from './page/Home'
+
+function App() {
+  return (
+
+    <Router>
+      <Switch>
+      <Route exact path="/">
+           <Home />
+      </Route>
+        
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
